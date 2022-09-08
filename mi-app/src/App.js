090dca169e79fbb7a41c47b0347f02da0abcf69a
Isabel Lecourtois <rocleco@gmail.com>
2022-09-07 21:54:33 -0500

    Agregué la lista de productos

--- mi-app/src/App.js
+++ mi-app/src/App.js
@@ -5,21 +5,16 @@ import Titulo from './components/Titulo';
 import ItemCount from "./components/ItemCount";
 
 function App() {
-  const lista = ["0-6 meses", "6-12 meses", "12-24 meses"];
-  const items = lista.map(items => (
-    <ItemListContainer valor={items} />
-  ));
 
 
   return (
     <div className='container'>
       {<Header />}
       <hr></hr>
-      <main className= "main-index">
+      <main className="main-index">
         {<Titulo valor="Nuestros Cursos" />}
-        {<ul className="list-inline">
-          {items}
-        </ul>}
+        <hr />
+        <ItemListContainer />
         <ItemCount stock={200} initial={1} onAdd={0} />
       </main>
     </div>
--- mi-app/src/components/ItemListContainer.jsx
+++ mi-app/src/components/ItemListContainer.jsx
@@ -1,12 +1,48 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import ItemList from "./ItemList";
+import bebe1 from "../images/bebe1.jpg";
+import bebe2 from "../images/bebe2.jpg";
+import bebe3 from "../images/bebe3.jpg";
+import bebe4 from "../images/bebe4.jpg";
 
-const ItemListContainer = (greeting) => {
-        return (
-            <li className="list-inline-item">
-                <a href="/">{greeting.valor}</a>
-            </li>
-        );
-    }
 
+
+const ItemListContainer = () => {
+    const [items, setItems] = useState([]);
+
+
+
+    useEffect(() => {
+        const productos = [
+            {"id":1, "nombre":"Hábitos Saludables (0-5 meses)", "descripcion":"Crea hábitos de sueño saludables para tu bebé y vuelve a dormir como antes.", "imagen":bebe1},
+            {"id":2, "nombre":"Regresión de los 4 meses (3-4 meses)", "descripcion":"Recupera el avance que llevabas con el sueño de tu bebé para que vuelva a dormir como antes.", "imagen":bebe2},
+            {"id":3, "nombre":"Durmiendo toda la Noche (5-18 meses)", "descripcion":"14 días para que tu bebé logre dormir hasta 12 horas decorrido por las noches.", "imagen":bebe3},
+            {"id":4, "nombre":"Planes de Sueño Personalizados (5meses-6años)", "descripcion":"Diseñamos el plan perfecto para que tu bebé y tu duerman toda la noche.", "imagen":bebe4},
+            {"id":5, "nombre":"Bye Bye Madrugadores (5+ meses)", "descripcion":"Corrige los despertares de tu bebé y evita que madrugue más que tú.", "imagen":bebe1}
+
+
+        ];
+
+        console.log (productos.map (item => item.imagen));
+
+        const getProductos = new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(productos);
+            }, 2000);
+        });
+
+        getProductos.then((respuesta) => {
+            setItems(respuesta);
+        });
+    }, []);
+
+    return (
+        <div className="container">
+            <ItemList items={items} />
+        </div>
+    )
+}
 
 export default ItemListContainer;
